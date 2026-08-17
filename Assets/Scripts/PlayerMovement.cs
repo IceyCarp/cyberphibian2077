2c4a1b78b90ad0b58d6cd91fa3e0cd02fe83e2f7
Icey_Carp <Dixenbv@gmail.com>
2026-08-17 10:47:17 +0200

    Player Movement
    Omni directional movement setup to input system and custom jump length

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,4 +8,37 @@ public class PlayerMovement : MonoBehaviour
 
     [Header("Components")]
     [SerializeField] PlayerInput input;
+    [SerializeField] private Transform playerTransform;
+
+    public void OnForward(InputAction.CallbackContext cxt)
+    {
+        if (cxt.started)
+        {
+            playerTransform.position = new Vector3(playerTransform.position.x, playerTransform.position.y, playerTransform.position.z + jumpLength);
+        }
+    }
+
+    public void OnBackwards(InputAction.CallbackContext cxt)
+    {
+        if (cxt.started)
+        {
+            playerTransform.position = new Vector3(playerTransform.position.x, playerTransform.position.y, playerTransform.position.z - jumpLength);
+        }
+    }
+
+    public void OnRight(InputAction.CallbackContext cxt)
+    {
+        if (cxt.started)
+        {
+            playerTransform.position = new Vector3(playerTransform.position.x + jumpLength, playerTransform.position.y, playerTransform.position.z);
+        }
+    }
+
+    public void OnLeft(InputAction.CallbackContext cxt)
+    {
+        if (cxt.started)
+        {
+            playerTransform.position = new Vector3(playerTransform.position.x - jumpLength, playerTransform.position.y, playerTransform.position.z);
+        }
+    }
 }
